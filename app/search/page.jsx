417cdc9bda8b4b848dcb963674bb59ee5a2e7009
David Okpe <okpedavid0@gmail.com>
2025-01-11 22:05:50 +0100

    first commit for the day

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -4,23 +4,26 @@ const Page = async(props) => {
     const {searchParams}= await props
     const query = await searchParams
     const q= query.q
-      const results = await fetchsearch(q)
-      console.log( 'test',results?.content?.results[0])
-
+    try {
+   const results = await fetchsearch(q)
+      
+    } catch (error) {
+      
+    }
   return (
     <div>
       <div className="p-10">
         <h1 className="text-3xl  font-bold mb-2">Results for {q}</h1>
         <h2 className="mb-5 text-gray-400">
-          ({results.content.page_details.total_results} results)
+          ({results?.content?.page_details?.total_results} results)
         </h2>
 
         <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
           {
-            results?.content?.results.map(product=>{
+            results?.content?.results?.map(product=>{
               console.log(product)
-              return <li className="" key={product.product_id}>
-                {product.general.title} pp
+              return <li className="" key={product.general.product_id}>
+                {product.general.title}
               </li>
             })
           }
